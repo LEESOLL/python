c63250c49029ca11700eb44c8453bf72c7a424a7
LEESOLL <57247409+LEESOLL@users.noreply.github.com>
2022-08-18 23:12:39 +0900

    Create Movie Comment Website
    using flask, requests, bs4, dnspython, pymongo

--- projects/mars/app.py
+++ projects/mars/app.py
@@ -1,19 +1,37 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.9pyyebx.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
 @app.route('/')
 def home():
    return render_template('index.html')
 
 @app.route("/mars", methods=["POST"])
 def web_mars_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+    doc = {
+        'name':name_receive,
+        'address':address_receive,
+        'size':size_receive
+    }
+    db.mars.insert_one(doc)
+
+    return jsonify({'msg':'주문 완료!'})
 
 @app.route("/mars", methods=["GET"])
 def web_mars_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    order_list = list(db.mars.find({}, {'_id': False}))
+
+    return jsonify({'orders':order_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
